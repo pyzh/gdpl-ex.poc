c4e7c08ab64941f8716a31de3be3fb590634424c
s6dyl <absente@live.cn>
2018-10-01 20:06:30 +0800

    pause: xiuxi, eat

--- Ld2e.exs
+++ Ld2e.exs
@@ -1,11 +1,11 @@
 defmodule Lib do
     # 文="字符串"
-    def 印 文 do: IO.puts 文 
-    def 拆 文,间隔 do: String.split 文, 间隔
-    def 文去 文,式 do: String.trim 文, 式 
-    def 文起 文,前 do: String.starts_with? 文, 前
-    def 文移 文,位 do: String.slice 文, 位, String.length(文)
-    def 转 名 when is_bitstring 名 do: String.to_atom 名
+    def 印 文 do IO.puts 文 end
+    def 拆 文,间隔 do String.split 文, 间隔 end
+    def 文去 文,式 do String.trim 文, 式 end
+    def 文起 文,前 do String.starts_with? 文, 前 end
+    def 文移 文,位 do String.slice 文, 位, String.length(文) end
+    def 转 名 when is_bitstring 名 do String.to_atom 名 end
 
     def draftHelp do
         #a = String.split("1,2", ",")
@@ -22,9 +22,9 @@ defmodule Lib do
     end
 end
 
-def DS do # data store
-    def 储 名 do: :ets.new 名,{:set,:protected} # 表
-    def 入 库,物 do: :ets.insert(库,物) # 物={名,实}
+defmodule DS do # data store
+    def 储 名 do :ets.new 名,{:set,:protected} end # 表
+    def 入 库,物 do :ets.insert(库,物) end # 物={名,实}
 end
 
 defmodule Ld2e do
@@ -37,15 +37,17 @@ defmodule Ld2e do
     @此 :Ld2e
 
     def 分 do import Lib
-        断句 = 拆 @指令1, @句 end
+        断句 = 拆 @指令1, @句 
+        特解 断句
+    end
     def 特解 句 do import DS
-        [一,二,三] = 断句
+        [一,二,三] = 句
         仓 = 储 @此
         生一 一
         生二 二
         生三 三
     end
-    def 通解 句 do: #略
+    def 通解 句 do end #略
 
     def 入 名,实 do import DS
         入 @此, {名,实}
@@ -59,11 +61,6 @@ defmodule Ld2e do
     end
     def 生一 言 do import Lib
         生 言,&(文去 &1,"'")
-        # 句一 = 拆 言, @令
-        # [名|实] = 句一
-        # 值 = 文去 实,"'"
-        # 键 = 转 名
-        # 入 键,值
     end
     def 生二 言 do import Lib
         @确定 = 文前 言,@记
---
+++ test.exs
@@ -0,0 +1,6 @@
+defmodule A do
+    #def run do:    x=1;    y=2;    x+y
+    def run do 1 end
+end
+
+IO.puts A.run
