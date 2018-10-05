a4324119bf897924f2e5c023efdbeda3feed4f7f
s6dyl <absente@live.cn>
2018-10-05 10:14:36 +0800

    sync

--- Ld2e2o.exs
+++ Ld2e2o.exs
@@ -1,42 +1,45 @@
-#可读性优化版   #参数用英文字母
-
-defmodule Ld2e do 
-    @句 "；" ; @言 "，" ; @令 "为" ; @求 "求" ; @记 "记"
-    @此 :Ld2e
-    import Lib ; import DS
-    @指令1 "List1为'1234234'；记a为List1，b为2；求a中b的个数"
+defmodule Ld2e do  @this :Ld2e
+    @cmd1 "List1为'1234234'；记a为List1，b为2；求a中b的个数"
     
-    def 开 do 分(); 印 "完" end
-
-    def 分 do 拆( @指令1, @句) |> 特解 end
-
-    def 特解 z do  _仓= 储 @此; z |> 生一 |> 生二 |> 生三 end
-
-    def 生 z, 核\\&(&1) do [a,b|_] = 拆 z, @令
-        k = 转 a; v = 核.(b)
-        入 @此, {k,v} end
-
-    def 生一 [z|t] do 生 z,&(文去 &1,"'"); t end
+    def run do split(); IO.puts "完" end
     
-    def 生二 [z|t] do true = 文起 z,@记
-        [z1,z2] = 文移(z,1) |> 拆(@言)
-        生 z1,&转/1; 生 z2;   t    end
+    def split do String.split(@cmd1, "；") |> sub end
 
-    def 生三 [z|_] do true = 文起 z,@求
-        kli = 列(@此,全键()) |> 循平
-        sli = 文移(z,1) |> 拆("") |> 组去(0) |> 组去(-1) |> 循映(&转/1)
-        vsli = for k<-sli, k in kli, do: {k, 解 k}
+    def sub z do  _table= :ets.new(@this, [:set,:protected,:named_table])
+        z |> s1 |> s2 |> s3 end
+
+    def s z, f\\&(&1) do 
+        [a,b|_] = String.split z, "为"
+        k = String.to_atom a; v = f.(b)
+        :ets.insert @this, {k,v} end
+
+    def s1 [z|t] do s z,&(String.trim &1,"'"); t end
+    
+    def s2 [z|t] do true = String.starts_with? z,"记"
+        [z1,z2] = String.slice(z,1,
+                    String.length(z)) |> String.split("，")
+        s z1,&String.to_atom/1; s z2;   t    end
+
+    def s3 [z|_] do true = String.starts_with? z,"求"
+        kli = :ets.match(@this,{:'$1',:_})
+                 |> Enum.flat_map((& &1))
+        sli = String.slice(z,1,String.length(z))
+           |> String.split("") 
+           |> List.delete_at(0) |> List.delete_at(-1)
+           |> Enum.map(&String.to_atom/1)
+        vsli = for k<-sli, k in kli, do: {k, find k}
         true = :中 in sli
-        {x,y} = 定位 转(sli), vsli, 组位(sli, :中)
-        if 文有 z, "个数" do
-            印 "A, B =", [x,y]
-            印 "A中B的个数= ",计数(x,y)
+        {x,y} = loc List.to_tuple(sli), vsli, Enum.find_index(sli,&(&1==:中))
+        if String.contains? z, "个数" do
+            IO.inspect [x,y], label: "A, B ="
+            IO.inspect Enum.count(
+                         String.split(x,""),&(&1==y)), label: "A中B的个数= "
         end
     end
-    def 定位 t, vlis, 中位 do 定 = &(取 vlis, (取 t,&1))
-        {定.(中位-1),定.(中位+1)} end
+    def loc t, vlis, mid do set = &(Keyword.get vlis, (elem t,&1))
+        {set.(mid-1),set.(mid+1)} end
 
-    def 解(a) when is_atom a do a2 = 取值(@此, a)
-        case is_atom a2 do true -> 解 a2; _ -> a2 end end
+    def find(a) when is_atom a do [{^a,a2}] = :ets.lookup(@this, a)
+        case is_atom a2 do true -> find a2; _ -> a2 end end
 end
-Ld2e.开
+Ld2e.run
